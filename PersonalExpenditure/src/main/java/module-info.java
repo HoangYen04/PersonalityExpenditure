a4219e2fdea16f75a7a833f016a01e2cb8bf918f
Yen <2251050084yen@ou.edu.vn>
2025-04-12 16:09:38 +0700

    commit envir...

---
+++ PersonalExpenditure/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.kym.personalexpenditure {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.base;
+
+    opens com.kym.personalexpenditure to javafx.fxml;
+    exports com.kym.personalexpenditure;
+    exports com.kym.pojo; 
+}
