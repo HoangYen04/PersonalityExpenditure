6a7d4e7f9ebdcd9b6eb2fa238c32d3420f4e0b9e
Yen <2251050084yen@ou.edu.vn>
2025-04-17 10:57:06 +0700

    update interface

--- PersonalExpenditure/src/main/java/com/kym/personalexpenditure/PrimaryController.java
+++ PersonalExpenditure/src/main/java/com/kym/personalexpenditure/PrimaryController.java
@@ -5,9 +5,9 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-//    @FXML
-//    private void switchToSecondary() throws IOException {
-//        App.setRoot("secondary");
-//    }
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
     
 }
